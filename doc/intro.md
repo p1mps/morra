1180f5e2e9093d174bc7619656ce8862b1d505bc
Andrea Imparato <imparato.andrea@gmail.com>
2018-10-14 22:10:32 +0200

    Add lein template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to morra
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/morra/core_test.clj
@@ -0,0 +1,7 @@
+(ns morra.core-test
+  (:require [clojure.test :refer :all]
+            [morra.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
