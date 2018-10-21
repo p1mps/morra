e022f8384888dd22aba59cd2d58de79f5a0db611
Andrea Imparato <imparato.andrea@gmail.com>
2018-10-21 18:20:51 +0200

    Add implementation with sync sockets

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[clj-sockets "0.1.0"]
+                 [org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot morra.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/morra/core.clj
+++ src/morra/core.clj
@@ -1,7 +1,47 @@
 (ns morra.core
   (:gen-class))
 
+(require '[clj-sockets.core :refer [close-socket write-line read-line listen create-server]])
+
+(def server (listen (create-server 9871)))
+
+(def data
+  (atom
+   {:player1-number 0
+    :player1-sum 0
+    :player2-number 0
+    :player2-sum 0}))
+
+(defn real-sum-players [data]
+  (+
+   (:player1-number data)
+   (:player2-number data)))
+
+(defn check-players [data]
+  (prn (str "Player1 Number: " (:player1-number @data)))
+  (prn (str "Player2 Number: " (:player2-number @data)))
+  (prn (str "Player1 Sum: " (:player1-sum @data)))
+  (prn (str "Player2 Sum: " (:player2-sum @data)))
+  (prn (str "Real sum is: " (real-sum-players @data)))
+  (when (= (:player1-sum @data) (real-sum-players @data))
+    (prn "Player1 Wins!"))
+  (when (= (:player2-sum @data) (real-sum-players @data))
+    (prn "Player2 Wins!")))
+
+(defn read-server [server]
+  (int (read-string (read-line server))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (write-line server "Insert player1 number")
+  (swap! data assoc :player1-number (read-server server))
+  (write-line server "Insert player1 sum")
+  (swap! data assoc :player1-sum (read-server server))
+  (write-line server "Insert player2 number")
+  (swap! data assoc :player2-number (read-server server))
+  (write-line server "Insert player2 sum")
+  (swap! data assoc :player2-sum (read-server server))
+  (check-players data)
+  )
+
